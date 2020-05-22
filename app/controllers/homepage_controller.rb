9aae508c45ec95c46a9edbf1f4d4a10b6d49be94
Antonia <antonianomamiukor@gmail.com>
2020-05-22 12:55:56 -0700

    modified homepage and works controller. modified respective views, link from homepage to works that are displayed by category

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,6 @@
 class HomepageController < ApplicationController
+  #TODO
   def index
+    @works = Work.all
   end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,18 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+  
+  def show
+    word_id = params[:id]
+    @work = Work.find_by(id: passenger_id)
+    
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
+  def new
+    @work = Work.new
+  end
 end
