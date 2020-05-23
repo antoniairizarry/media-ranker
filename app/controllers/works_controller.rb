ede0310d8aa015a66e036c67823b54d7d6d59aa8
Antonia <antonianomamiukor@gmail.com>
2020-05-23 00:33:01 -0700

    updated work controller actions and corresponding views. Able to edit, delete, and add works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,10 +4,10 @@ class WorksController < ApplicationController
   end
   
   def show
-    word_id = params[:id]
-    @work = Work.find_by(id: passenger_id)
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
     
-    if @passenger.nil?
+    if @work.nil?
       head :not_found
       return
     end
@@ -15,4 +15,48 @@ class WorksController < ApplicationController
   def new
     @work = Work.new
   end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(@work.id)
+      return
+    else
+      render :new, status: :bad_request
+      return
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.update(work_params)
+      redirect_to work_path
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    work_id = params[:id]
+    @work = Work.find_by_id(work_id)
+
+    if @work.nil?
+      head :not_found
+      return
+    end
+    @work.destroy
+
+    redirect_to works_path
+    return
+  end
+end
+
+end
+
+def work_params
+  return params.require(:work).permit(:category, :title, :artist, :published, :description)
 end
